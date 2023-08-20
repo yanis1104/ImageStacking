7b328a38d15b50f32ad580dec516665219f2253a
Yanis Gougeat <yanis.gougeat@epitech.eu>
2023-08-20 17:50:05 +0200

    gitignore and use of cropper video for faster tests

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import ImageLoader
 
-path = 'videos/video.ser'
+path = 'videos/video_crop.ser'
 
 frames = ImageLoader.analyse_video(path)
 # frames is a dict which associates the index of a frame to its variance. The dict is then sorted by highest variance
