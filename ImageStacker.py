d0617e3bd797a25e44679956b86f6c0bf7e41ccb
Yanis Gougeat <yanis.gougeat@epitech.eu>
2024-01-06 21:21:15 +0100

    .feat: video lopping after reading the last frame. creation of a function to read a specific frame according to the current position of the slider

--- ImageStacker.py
+++ ImageStacker.py
@@ -8,6 +8,8 @@ from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QImage, QPixmap
 
+import Video
+
 #def old():
     ## Chemin vers le dossier contenant les images
     #images_folder = 'video_crop'
@@ -45,6 +47,8 @@ class GUI(QMainWindow):
         self.video_is_paused = True
         self.video_label = QLabel(self)
         self.frame_index = 0
+        self.cursor_moved_by_user = False
+        self.closeProgram = False
 
         self.initGUI()
 
@@ -52,14 +56,7 @@ class GUI(QMainWindow):
 
 
     def openFile(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.ReadOnly
-        self.filename, _ = QFileDialog.getOpenFileName(self, 'Ouvrir', '', 'SER Files (*.ser);;MP4 Files (*.mp4);;All Files (*)', options=options)
-
-        if self.filename:
-            self.video = cv2.VideoCapture(self.filename)
-            self.readVideo()
-            self.frame_index = 1
+        pass
 
     def createOpenFileButton(self, data_load_layout :QVBoxLayout):
         # Création d'un bouton
@@ -70,7 +67,7 @@ class GUI(QMainWindow):
         data_load_layout.addWidget(open_file_button)
 
     def playVideo(self):
-        self.video_is_paused = not self.video_is_paused
+        pass
 
     def createPlayButton(self, data_video_layout :QVBoxLayout):
         # Création d'un bouton
@@ -82,68 +79,41 @@ class GUI(QMainWindow):
         data_video_layout.addWidget(self.play_video_button)
 
     def accessFrameIndex(self):
-        self.frame_index = self.trackbar.value()
-        if not self.video.isOpened():
-            QMessageBox.information(self, 'Error', 'Cannot open file')
-            return
-        self.video.set(cv2.CAP_PROP_POS_FRAMES, self.frame_index - 1)
-        self.showFrameVideo()
+        if self.cursor_moved_by_user:
+            self.frame_index = self.trackbar.value()
+            if not self.video.isOpened():
+                QMessageBox.information(self, 'Error', 'Cannot open file')
+                return
+            self.video.set(cv2.CAP_PROP_POS_FRAMES, self.frame_index)
+            self.showFrameVideo()
+
+    def play(self):
+        self.cursor_moved_by_user = False
+
+    def pause(self):
+        self.cursor_moved_by_user = True
 
     def createTrackBar(self, data_video_layout :QVBoxLayout):
         self.trackbar = QSlider(self)
         self.trackbar.setOrientation(1)
         self.trackbar.setMinimum(0)
         self.trackbar.setMaximum(0)
-        self.trackbar.valueChanged.connect(self.accessFrameIndex)
+        self.trackbar.sliderMoved.connect(self.accessFrameIndex)
+        self.trackbar.sliderPressed.connect(self.pause)
+        self.trackbar.sliderReleased.connect(self.play)
 
         data_video_layout.addWidget(self.trackbar)
 
+    def updateIndex(self):
+        if self.frame_index < self.video_len - 1:
+            self.frame_index += 1
+            self.trackbar.setValue(self.frame_index)
+
     def showFrameVideo(self):
-        ret, frame = self.video.read()
+        pass
 
-        # Si la lecture de la vidéo est terminée
-        if not ret:
-            return 0
-
-        print(self.frame_index)
-        self.frame_index += 1
-        self.trackbar.setValue(self.frame_index - 1)
-
-        frame = cv2.resize(frame, None, fx=self.video_size_factor, fy=self.video_size_factor)
-
-        # Convertir l'image OpenCV en QImage
-        height, width, channel = frame.shape
-        bytesPerLine = 3 * width
-        qImg = QImage(frame.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
-
-        # Afficher l'image dans le QLabel
-        pixmap = QPixmap.fromImage(qImg)
-        self.video_label.setPixmap(pixmap)
-
-        # Rafraîchir l'affichage
-        QApplication.processEvents()
-
-        if not ret:
-            return 0
-        return 1
-
-    def readVideo(self):
-        if not self.video.isOpened():
-            QMessageBox.information(self, 'Error', 'Cannot open file')
-            return
-
-        self.play_video_button.setEnabled(True)
-        self.video_len = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.trackbar.setMinimum(1)
-        self.trackbar.setMaximum(self.video_len)
-        
-        while True:
-            # Si video en pause
-            while self.video_is_paused:
-                QApplication.processEvents()
-            if self.showFrameVideo() == 0:
-                break
-            
+    def openVideo(self):
+        pass
 
     def initGUI(self):
         self.setWindowTitle('Image Stacker')
@@ -184,9 +154,14 @@ class GUI(QMainWindow):
         if self.video and self.video.isOpened():
             self.video.release()
             self.video_is_paused = False
-            self.play_video_button.setEnabled(False)
+            self.closeProgram = True
 
         event.accept()
+
+GUI.openFile = Video.openFile
+GUI.openVideo = Video.openVideo
+GUI.showFrameVideo = Video.showFrameVideo
+GUI.playVideo = Video.playVideo
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
---
+++ Video.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+import os
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QMessageBox, QFileDialog, QLabel, QSlider
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QImage, QPixmap
+
+def openFile(self):
+    options = QFileDialog.Options()
+    options |= QFileDialog.ReadOnly
+    self.filename, _ = QFileDialog.getOpenFileName(self, 'Ouvrir', '', 'SER Files (*.ser);;MP4 Files (*.mp4);;All Files (*)', options=options)
+
+    if self.filename:
+        self.video = cv2.VideoCapture(self.filename)
+        self.openVideo()
+        self.trackbar.setValue(0)
+        self.showFrameVideo()
+        self.updateIndex()
+
+def openVideo(self):
+    if not self.video.isOpened():
+        QMessageBox.information(self, 'Error', 'Cannot open file')
+        return
+
+    self.play_video_button.setEnabled(True)
+    self.video_len = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
+    self.trackbar.setMinimum(0)
+    self.trackbar.setMaximum(self.video_len - 1)
+
+def playVideo(self):
+    self.video_is_paused = not self.video_is_paused
+    if not self.video_is_paused:
+        self.play_video_button.setText("Pause")
+        while not self.closeProgram:
+        # Si video en pause
+            while self.video_is_paused:
+                QApplication.processEvents()
+            if self.showFrameVideo() == 0:
+                self.trackbar.setValue(0)
+            else:
+                self.updateIndex()
+                
+    self.play_video_button.setText("Play")
+
+def showFrameVideo(self):
+    ret, frame = self.video.read()
+
+    # Si la lecture de la vidéo est terminée
+    if not ret:
+        self.frame_index = 0
+        self.video.set(cv2.CAP_PROP_POS_FRAMES, self.frame_index)
+        return 0
+
+    frame = cv2.resize(frame, None, fx=self.video_size_factor, fy=self.video_size_factor)
+
+    # Convertir l'image OpenCV en QImage
+    height, width, channel = frame.shape
+    bytesPerLine = 3 * width
+    qImg = QImage(frame.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+
+    # Afficher l'image dans le QLabel
+    pixmap = QPixmap.fromImage(qImg)
+    self.video_label.setPixmap(pixmap)
+
+    # Rafraîchir l'affichage
+    QApplication.processEvents()
+
+    if not ret:
+        self.frame_index = 0
+        self.video.set(cv2.CAP_PROP_POS_FRAMES, self.frame_index)
+        return 0
+    return 1
